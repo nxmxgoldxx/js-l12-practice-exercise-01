c2a619b89ea6c7f36fba9d46cf1e9ec6fb04ec3b
nxmxgoldxx <nxmxgoldxx@live.com>
2021-11-04 10:54:25 -0700

    Practice Updated

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 const randomFolks = document.querySelector(".random-peeps");
+const getData = async function() {
+    const usersRequest = await fetch("https://randomuser.me/api?results5");
+    const data = await usersRequest.json();
+
+    const userResults = data.results; // object array
+    displayUsers(userResults);
+
+};
+
+getData();
+
+const displayUsers = function (userResults) {
+    // Clear the randomFolks element 
+    randomFolks.innerHTML = "";
+
+    for (const user of userResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageUrl = user.picture.medium;
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageUrl} alt="User avatar" />
+        `;
+        randomFolks.append(userDiv);
+    }
+};
